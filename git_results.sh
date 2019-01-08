5363722cfa80f5b18183b075ab0513e691dd3c49
zhu changpeng <zcp@master.zcp-qv45108.infosphere.emulab.net>
2019-01-08 18:37:09 -0500

    a commit

--- git_results.sh
+++ git_results.sh
@@ -2,5 +2,6 @@
 git init
 git add .
 git commit -m "a commit"
+git pull --rebase
 git remote add origin https://github.com/zcp/emulab-rubbos-result.git
 git push -u https://zcp:324zq999@github.com/zcp/emulab-rubbos-result.git
